2ba84bd9696e3eefcbb1dec093e7fb659a915df7
amosnistrian <amosnistrian@gmail.com>
2017-08-30 13:26:02 -0700

    initial

---
+++ script.js
@@ -0,0 +1,4 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+const FACE = document.querySelector("#FACE");
